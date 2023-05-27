f49de3163f0faaea8cee86922816baa04f7ffb6c
rauthan123 <38749021+rauthan123@users.noreply.github.com>
2023-05-27 12:15:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+import operator
+import os
+import pickle
+from collections import defaultdict
+
+import numpy as np
+import scipy.io.wavfile as wav
+from python_speech_features import mfcc
+
+dataset = []
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == predictions[x]:
+            correct += 1
+    return 1.0 * correct / len(testSet)
+
+def loadDataset(filename):
+    with open("my.dat", 'rb') as f:
+        while True:
+            try:
+                dataset.append(pickle.load(f))
+            except EOFError:
+                f.close()
+                break
+
+
+loadDataset("my.dat")
+
+
+def distance(instance1, instance2, k):
+    distance = 0
+    mm1 = instance1[0]
+    cm1 = instance1[1]
+    mm2 = instance2[0]
+    cm2 = instance2[1]
+    distance = np.trace(np.dot(np.linalg.inv(cm2), cm1))
+    distance += (np.dot(np.dot((mm2 - mm1).transpose(), np.linalg.inv(cm2)), mm2 - mm1))
+    distance += np.log(np.linalg.det(cm2)) - np.log(np.linalg.det(cm1))
+    distance -= k
+    return distance
+
+
+def getNeighbors(trainingSet, instance, k):
+    distances = []
+    for x in range(len(trainingSet)):
+        dist = distance(trainingSet[x], instance, k) + distance(instance, trainingSet[x], k)
+        distances.append((trainingSet[x][2], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+
+def nearestClass(neighbors):
+    classVote = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x]
+        if response in classVote:
+            classVote[response] += 1
+        else:
+            classVote[response] = 1
+    sorter = sorted(classVote.items(), key=operator.itemgetter(1), reverse=True)
+    return sorter[0][0]
+
+
+results = defaultdict(int)
+i = 1
+for folder in os.listdir("./music/"):
+    results[i] = folder
+    i += 1
+(rate, sig) = wav.read("blues.00017.wav")
+mfcc_feat = mfcc(sig, rate, winlen=0.020, appendEnergy=False)
+covariance = np.cov(np.matrix.transpose(mfcc_feat))
+mean_matrix = mfcc_feat.mean(0)
+feature = (mean_matrix, covariance, 0)
+pred = nearestClass(getNeighbors(dataset, feature, 5))
+print(pred)
+predictions = []
+leng = len(dataset)
+for x in range(leng):
+    predictions.append(nearestClass(getNeighbors(dataset, feature, 5)))
+accuracy1 = getAccuracy(dataset, predictions)
+print(accuracy1)
+print(results)
+print(results[pred])
